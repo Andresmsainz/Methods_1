9b80acdc0028ccbb59d0cb33eee1f32b0c34dfe8
Andres Sainz <andresmsainz@gmail.com>
2021-05-04 00:06:06 -0400

    Added Return Methods

--- Program.cs
+++ Program.cs
@@ -12,17 +12,25 @@ namespace Methods_1
             int addresult = 0;
 
             c = 15;
-            d = 10; 
-            addresult = c + d;
+            d = 10;
+            addresult = PerformAddition(c, d);
             Console.WriteLine($"{c} + {d} = {addresult}");
             DisplayMessage();
 
             a = 5;
             b = 3;
-            addresult = a + b;
+            addresult = PerformAddition(a, b);
             Console.WriteLine($"{a} + {b} = {addresult}");
             DisplayMessage();
-            
+
+
+        }
+
+        static int PerformAddition(int x, int y)
+        {
+            int result = 0;
+            result = x + y;
+            return result;
         }
         static void DisplayMessage()
         {
